69d7a34d70f6e6816b906f90d610fea4f9903de5
Yaro <yaro@localhost.localdomain>
2018-08-22 11:23:33 +0200

    added one additional line to check if it is in sync

--- hello.cpp
+++ hello.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main(void) {
+  cout << "Let's see how it works\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-
+  
   cout << "I have successfully built and run\n";
   return 0;
 }
