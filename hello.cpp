6a811549a99ccc1432cb203750a9e1fe8f016e31
Yaro <yaro@localhost.localdomain>
2018-08-23 14:07:35 +0200

    updated as well done

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,8 @@ int main(void) {
   cout << "Let's see how it works\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Let's see if the CI process works well\n";
+  cout << "Let's see if the process works well\n";
+  cout << "It works!!!\n"
   cout << "I have successfully built and run\n";
   return 0;
 }
